0f328cc768b271952421905ce92bc945c30895bf
Shen <shen.zhang0907@gmail.com>
2025-01-22 18:01:33 -0500

    created schema and seed data

---
+++ server/seed.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const User = require("./models/userSchema");
+const Todo = require("./models/todoSchema");
+const connectDB = require("./config/db");
+
+connectDB();
+const seedDB = async () => {
+  try {
+    await User.deleteMany({});
+    await Todo.deleteMany({});
+    console.log("Cleaned data");
+
+    const users = await User.insertMany([
+      {
+        userName: "zsy",
+        password: "lz2002111",
+        userInfo: {
+          email: "xxx@gmail.com",
+          firstName: "Shen",
+          lastName: "Zhang",
+        },
+      },
+    ]);
+
+    const todos = await Todo.insertMany([
+      {
+        userId: users[0]._id,
+        title: "First todo",
+        desc: "I dont know what to do",
+        completed: false,
+      },
+      {
+        userId: users[0]._id,
+        title: "Second todo",
+        desc: "I dont know what to doI dont know what to doI dont know what to doI dont know what to do",
+        completed: false,
+      },
+    ]);
+
+    mongoose.connection.close();
+  } catch (err) {
+    console.error("Error seeding the database: ", err);
+    mongoose.connection.close();
+  }
+};
+
+seedDB();
